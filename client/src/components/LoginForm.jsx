ca1be20027a127b2bf4fe87a729081fccc41f548
Eli Craig <elidcraig@gmail.com>
2022-07-20 21:26:06 -0700

    login form, fetch request

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Stack, Input, Button } from '@chakra-ui/react'
+import React, { useState } from 'react';
+import { Stack, Input, Button, FormControl, FormLabel } from '@chakra-ui/react'
 
 function LoginForm() {
+  const [formData, setFormData] = useState({ username: '', password: '' })
+
+  const handleChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+  const handleLogin = e => {
+    e.preventDefault()
+    
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    })
+    .then(res => {
+      if (res.ok) {
+        res.json().then(user => console.log(user))
+      } else {
+        res.json().then(errors => console.error(errors))
+      }
+    })
+  }
+
   return (
-    <Stack>
-      <Input type='text'/>
-      <Input type='password'/>
-      <Button>LOGIN</Button>
-    </Stack>
+    <form onSubmit={ handleLogin }>
+      <Stack>
+        <FormControl isRequired>
+          <FormLabel>Username</FormLabel>
+          <Input type='text' name='username' onChange={ handleChange }/>
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input type='password' name='password' onChange={ handleChange }/>
+        </FormControl>
+        <Button type='submit'>LOGIN</Button>
+      </Stack>
+    </form>
   );
 }
 
