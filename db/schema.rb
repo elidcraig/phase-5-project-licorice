e50197cdc1ba6911d1cf5bec666290998e14f38c
Eli Craig <elidcraig@gmail.com>
2022-07-21 13:44:16 -0700

    artist and release migrations up

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_20_223239) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_204329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "image_url"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "releases", force: :cascade do |t|
+    t.bigint "artist_id", null: false
+    t.string "title"
+    t.string "release_date"
+    t.string "artwork_url"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_releases_on_artist_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -25,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_20_223239) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "releases", "artists"
 end
