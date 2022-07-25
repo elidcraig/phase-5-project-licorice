8951eeb669445ba9d28f58a4b05b1dae30f52420
Eli Craig <elidcraig@gmail.com>
2022-07-25 09:25:30 -0700

    genres sent with artist and release

--- app/serializers/artist_serializer.rb
+++ app/serializers/artist_serializer.rb
@@ -1,4 +1,8 @@
 class ArtistSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image_url, :bio
+  attributes :id, :name, :image_url, :bio, :genre_list
   has_many :releases
+
+  def genre_list
+    object.genres.pluck(:name)
+  end
 end
--- app/serializers/release_serializer.rb
+++ app/serializers/release_serializer.rb
@@ -1,4 +1,8 @@
 class ReleaseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :release_date, :artwork_url, :release_type
+  attributes :id, :title, :release_date, :artwork_url, :release_type, :genre_list
   has_one :artist
+
+  def genre_list
+    object.genres.pluck(:name)
+  end
 end
