f5cfaa1156c968eccf4896997f09a429c1abac20
Eli Craig <elidcraig@gmail.com>
2022-07-25 10:21:21 -0700

    validations to prevent duplicate favorites

--- app/models/favorite_artist.rb
+++ app/models/favorite_artist.rb
@@ -1,4 +1,6 @@
 class FavoriteArtist < ApplicationRecord
   belongs_to :user
   belongs_to :artist
+
+  validates :user, uniqueness: { scope: :artist, message: 'can only add item to favorites once' }
 end
--- app/models/favorite_release.rb
+++ app/models/favorite_release.rb
@@ -1,4 +1,6 @@
 class FavoriteRelease < ApplicationRecord
   belongs_to :user
   belongs_to :release
+
+  validates :user, uniqueness: { scope: :release, message: 'can only add item to favorites once' }
 end
